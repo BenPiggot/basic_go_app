a13625321de222f3b4cc787c12afcd2e320cfdec
ben <wbpiggot@gmail.com>
2017-10-15 21:53:54 -0700

    wip

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-  "html/template"
 )
 
 func main() {
@@ -12,18 +12,18 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
-  mux.HandleFunc("/err", err)
+	mux.HandleFunc("/err", err)
 
-  mux.HandleFunc("/login", login)
-  mux.HandleFunc("/logout", logout)
-  mux.HandleFunc("/signup", signup)
-  mux.HandleFunc("/signup_account", signupAccount)
-  mux.HandleFunc("/authenticate", authenticate)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
 
-  mux.HandleFunc("/thread/new", newThread)
-  mux.HandleFunc("/thread/create", createThread)
-  mux.HandleFunc("/thread/post", postThread)
-  mux.HandleFunc("/thread/read", readThread) 
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -32,24 +32,14 @@ func main() {
 	server.ListenAndServe()
 }
 
-func index(w http.ResponseWriter, *http.Request) {
-  threads, err := data.Threads(); if err == nil {
-    _, err := session(w, r)
-    public_tmpl_files := []string{"templates/layout.html",
-                      "templates/public.navbar.html",
-                      "templates/index.html",}
-    private_tmpl_files := []string{"templates/layout.html",
-                      "templates/private.navbar.html",
-                      "templates/index.html",}
-
-    var templates *template.Template
-
-    if err != nil {
-      templates = template.Must(template.ParseFiles(private_tmpl_files...))
-    } else {
-      templates = template.Must(template.ParseFiles(public_tmpl_files...))
-    }
-
-    templates.ExecuteTemplate(w, "layout", threads)
-  }                    
+func index(writer http.ResponseWriter, request *http.Request) {
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(writer, request)
+		if err != nil {
+			generateHTML(writer, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(writer, threads, "layout", "private.navbar", "index")
+		}
+	}
 }
--- util.go
+++ util.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "net/http"
+  "fmt"
+  "html/template"
 )
 
 func session(w http.ResponseWriter, r *http.Request) (sess data.Session err error) {
@@ -13,4 +15,13 @@ func session(w http.ResponseWriter, r *http.Request) (sess data.Session err erro
     }
   }
   return
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+  var files []string
+  for _, file := range fn {
+    files = append(files, fmt.Sprintf("templates/%s.html", file))
+  }
+  templates := template.Must(template.ParseFiles(files...))
+  templates.ExecuteTemplate(writer, "layout", data)
 }
