51cee80429f62efe3b9054e26aa1509b6fc70aa4
ben <wbpiggot@gmail.com>
2017-10-23 16:06:04 -0700

    wip

---
+++ data/user.go
@@ -0,0 +1,38 @@
+package data
+
+import (
+	"time"
+)
+
+type User struct {
+	Id        int
+	Uuid      string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+}
+
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+// Get the session for an existing user
+func (user *User) Session() (session Session, err error) {
+	session = Session{}
+	rows, err = Db.Query("SELECT id, uuid, email, user_id, created_at FROM sessions WHERE user_id = $1", user.Id).
+		Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+// Get the user from the session
+func (session *Session) User() (user User, err error) {
+	user = User{}
+	rows, err = Db.Query("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", session.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
